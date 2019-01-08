47cf82d5cfb11deaeb10f744677ef65b325244d8
shahin1290 <s.patowary@yahoo.com>
2019-01-08 09:45:44 +0100

    Creates logic module BMICalculator, uses the module in DispalyResult component

--- src/DisplayResult.js
+++ src/DisplayResult.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { bmiCalculation } from './BMICalculator';
+
 
 class DisplayResult extends Component {
 
     calculate() {
         var weight= this.props.weight;
         var height= this.props.height;
+
+        return bmiCalculation(weight, height);
     }
     
     render() {
